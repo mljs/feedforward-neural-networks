5a0660f969f127925a8c98717e244546f5810ee1
Jefferson <swrt_9950@hotmail.com>
2015-06-16 16:32:57 +0200

    index.js added and some test with XOR.

---
+++ src/index.js
@@ -0,0 +1,1 @@
+exports.FeedforwardNeuralNetwork = require('./feedforwardNeuralNetwork');
--- test/test.js
+++ test/test.js
@@ -1,16 +1,19 @@
 "use strict";
 
-var FeedforwardNeuralNetwork =require("../src/feedforwardNeuralNetworks");
+var FeedforwardNeuralNetwork =require("../src/feedforwardNeuralNetwork");
 
 describe('Feedforward Neural Networks', function () {
     it('Training the neural network with XOR', function () {
         var trainingSet = [[0, 0], [0, 1], [1, 0], [1, 1]];
         var predictions = [0, 1, 1, 0];
 
-        var xorNN = new FeedforwardNeuralNetwork(2, [2, 2]);
+        var xorNN = new FeedforwardNeuralNetwork(2, [2, 1]);
         xorNN.layers[1].isSigmoid = false;
         xorNN.train(trainingSet, predictions, 500, 0.3, 0.3);
-        console.log(xorNN.forwardNN([1, 0]));
-        false.should.be.ok;
+        xorNN.forwardNN([0, 0]).should.be.approximately(0, 1e-1);
+        xorNN.forwardNN([0, 1]).should.be.approximately(1, 1e-1);
+        xorNN.forwardNN([1, 0]).should.be.approximately(1, 1e-1);
+        xorNN.forwardNN([1, 1]).should.be.approximately(0, 1e-1);
+
     });
 });
