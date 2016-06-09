0d9b4cb39cbb65835823cff4a30c97549a0d3c38
Michaël Zasso <targos@protonmail.com>
2016-06-09 16:25:53 +0200

    refactor: classify Layer

--- src/layer.js
+++ src/layer.js
@@ -2,6 +2,79 @@
 
 var Matrix = require("ml-matrix");
 
+class Layer {
+    /**
+     * Constructor that creates a layer for the neural network given the number of inputs
+     * and outputs.
+     * @param inputSize
+     * @param outputSize
+     * @constructor
+     */
+    constructor(inputSize, outputSize) {
+        this.output = Matrix.zeros(1, outputSize).getRow(0);
+        this.input = Matrix.zeros(1, inputSize + 1).getRow(0); //+1 for bias term
+        this.deltaWeights = Matrix.zeros(1, (1 + inputSize) * outputSize).getRow(0);
+        this.weights = randomInitializeWeights(this.deltaWeights.length, inputSize, outputSize);
+        this.isSigmoid = true;
+    }
+
+    /**
+     * Function that performs the forward propagation for the current layer
+     * @param {Array} input - output from the previous layer.
+     * @returns {Array} output - output for the next layer.
+     */
+    forward(input) {
+        this.input = input.slice();
+        this.input.push(1); // bias
+        var offs = 0; // offset used to get the current weights in the current perceptron
+        this.output = Matrix.zeros(1, this.output.length).getRow(0);
+
+        for (var i = 0; i < this.output.length; ++i) {
+            for (var j = 0; j < this.input.length; ++j) {
+                this.output[i] += this.weights[offs + j] * this.input[j];
+            }
+            if (this.isSigmoid)
+                this.output[i] = sigmoid(this.output[i]);
+
+            offs += this.input.length;
+        }
+
+        return this.output.slice();
+    }
+
+    /**
+     * Function that performs the backpropagation algorithm for the current layer.
+     * @param {Array} error - errors from the previous layer.
+     * @param {Number} learningRate - Learning rate for the actual layer.
+     * @param {Number} momentum - The regularizarion term.
+     * @returns {Array} the error for the next layer.
+     */
+    train(error, learningRate, momentum) {
+        var offs = 0;
+        var nextError = Matrix.zeros(1, this.input.length).getRow(0);//new Array(this.input.length);
+
+        for (var i = 0; i < this.output.length; ++i) {
+            var delta = error[i];
+
+            if (this.isSigmoid)
+                delta *= sigmoidGradient(this.output[i]);
+
+            for (var j = 0; j < this.input.length; ++j) {
+                var index = offs + j;
+                nextError[j] += this.weights[index] * delta;
+
+                var deltaWeight = this.input[j] * delta * learningRate;
+                this.weights[index] += this.deltaWeights[index] * momentum + deltaWeight;
+                this.deltaWeights[index] = deltaWeight;
+            }
+
+            offs += this.input.length;
+        }
+
+        return nextError;
+    }
+}
+
 module.exports = Layer;
 
 /**
@@ -40,74 +113,3 @@ function sigmoid(value) {
 function sigmoidGradient(value) {
     return value * (1 - value);
 }
-
-/**
- * Constructor that creates a layer for the neural network given the number of inputs
- * and outputs.
- * @param inputSize
- * @param outputSize
- * @constructor
- */
-function Layer(inputSize, outputSize) {
-    this.output = Matrix.zeros(1, outputSize).getRow(0);
-    this.input = Matrix.zeros(1, inputSize + 1).getRow(0); //+1 for bias term
-    this.deltaWeights = Matrix.zeros(1, (1 + inputSize) * outputSize).getRow(0);
-    this.weights = randomInitializeWeights(this.deltaWeights.length, inputSize, outputSize);
-    this.isSigmoid = true;
-}
-
-/**
- * Function that performs the forward propagation for the current layer
- * @param {Array} input - output from the previous layer.
- * @returns {Array} output - output for the next layer.
- */
-Layer.prototype.forward = function (input) {
-    this.input = input.slice();
-    this.input.push(1); // bias
-    var offs = 0; // offset used to get the current weights in the current perceptron
-    this.output = Matrix.zeros(1, this.output.length).getRow(0);
-
-    for(var i = 0; i < this.output.length; ++i) {
-        for(var j = 0 ; j < this.input.length; ++j) {
-            this.output[i] += this.weights[offs + j] * this.input[j];
-        }
-        if(this.isSigmoid)
-            this.output[i] = sigmoid(this.output[i]);
-
-        offs += this.input.length;
-    }
-
-    return this.output.slice();
-};
-
-/**
- * Function that performs the backpropagation algorithm for the current layer.
- * @param {Array} error - errors from the previous layer.
- * @param {Number} learningRate - Learning rate for the actual layer.
- * @param {Number} momentum - The regularizarion term.
- * @returns {Array} the error for the next layer.
- */
-Layer.prototype.train = function (error, learningRate, momentum) {
-    var offs = 0;
-    var nextError = Matrix.zeros(1, this.input.length).getRow(0);//new Array(this.input.length);
-
-    for(var i = 0; i < this.output.length; ++i) {
-        var delta = error[i];
-
-        if(this.isSigmoid)
-            delta *= sigmoidGradient(this.output[i]);
-
-        for(var j = 0; j < this.input.length; ++j) {
-            var index = offs + j;
-            nextError[j] += this.weights[index] * delta;
-
-            var deltaWeight = this.input[j] * delta * learningRate;
-            this.weights[index] += this.deltaWeights[index] * momentum + deltaWeight;
-            this.deltaWeights[index] = deltaWeight;
-        }
-
-        offs += this.input.length;
-    }
-
-    return nextError;
-};
